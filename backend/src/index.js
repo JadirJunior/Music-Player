8f747cff5822b6b99e65523d131038281e9b2f38
JadirJunior <jadirjunior8@gmail.com>
2020-08-10 12:26:21 -0300

    Server

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+
+app.listen(3333);
+
